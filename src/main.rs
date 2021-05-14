824e87ed3a86d1afc03c749c849bfc8f7a8bbc7f
km3ll <kmell@protonmail.com>
2021-05-14 18:18:56 -0500

    Print available docker commands

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
-mod module_03;
+use std::process::{Command};
 
 fn main() {
+
     println!("hello, world!");
+
+    let mut cmd = Command::new(String::from("docker"));
+    cmd.arg("--help");
+
+    let output = cmd.output().expect("Error getting output");
+    let result: String = String::from_utf8(output.stdout).expect("Error converting from utf8");
+
+    let mut lines = result.lines().map(|s| s.trim());
+
+    while let Some(line) = lines.next() {
+        match line {
+            "Commands:" => {
+                while let Some(cmd) = lines.next() {
+                    if cmd == "" {
+                        break;
+                    } else {
+                        println!("{}", cmd);
+                    }
+                }
+            }
+            _ => continue
+        }
+    }
+
 }
